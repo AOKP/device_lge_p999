a62cd680608eeedb85246fe2fa49113a3366b1db
Jonathan Steadman <xxwhitehawk@gmail.com>
2012-03-10 10:41:20 -0800

    change config path

--- aokp.mk
+++ aokp.mk
@@ -1,8 +1,8 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/aokp/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/p999/p999.mk)
